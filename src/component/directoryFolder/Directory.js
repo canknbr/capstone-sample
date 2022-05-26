46605f0e7d43cf76574e6a898c671e0bb9412af6
Can Kanbur <cankanbur1@gmail.com>
2022-05-26 17:08:41 +0300

    updating diretory folder

--- src/component/directoryFolder/Directory.js
+++ src/component/directoryFolder/Directory.js
@@ -1,36 +1,10 @@
 import React from 'react';
 import CategoryItem from '../categoryItem/CategoryItem.js';
-import '../../categories.styles.scss';
-const categories = [
-  {
-    id: 1,
-    title: 'hats',
-    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-  },
-  {
-    id: 2,
-    title: 'jackets',
-    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-  },
-  {
-    id: 3,
-    title: 'sneakers',
-    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-  },
-  {
-    id: 4,
-    title: 'womens',
-    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-  },
-  {
-    id: 5,
-    title: 'mens',
-    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-  },
-];
-const Directory = () => {
+import './directory.styles.scss';
+
+const Directory = ({ categories }) => {
   return (
-    <div className="categories-container">
+    <div className="directory-container">
       {categories.map(category => {
         return <CategoryItem {...category} />;
       })}
