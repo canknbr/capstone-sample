910ea0026cc557cb81f1271024a246b8527e2b81
Can Kanbur <cankanbur1@gmail.com>
2022-05-26 16:45:30 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-function App() {
-  return <div className="App"></div>;
-}
+import React from 'react';
+import Directory from './component/directoryFolder/Directory';
+const App = () => {
+  return <Directory />;
+};
 
 export default App;
---
+++ src/component/categoryItem/CategoryItem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './category.styles.scss';
+
+const CategoryItem = ({ id, imageUrl, title }) => {
+  return (
+    <div className="category-container" key={id}>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      ></div>
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>shop now</p>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryItem;
