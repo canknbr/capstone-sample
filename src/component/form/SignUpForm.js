6a40be2d39d93caecf48fe112a723dffd1413739
Can Kanbur <cankanbur1@gmail.com>
2022-06-08 15:47:27 +0300

    .

--- src/component/form/SignUpForm.js
+++ src/component/form/SignUpForm.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
-
+import {
+  createAuthUserWithEmailAndPassword,
+  createUserDocumentFromAuth,
+} from '../../utils/firebase/firebase.js';
 const defaultValue = {
   displayName: '',
   email: '',
@@ -9,16 +12,39 @@ const defaultValue = {
 const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultValue);
   const { displayName, email, password, confirmPassword } = formFields;
+  const resetForm = () => {
+    setFormFields(defaultValue);
+  };
   const handleChange = e => {
     const { name, value } = e.target;
     setFormFields(prev => {
       return { ...prev, [name]: value };
     });
   };
+  const handleSubmit = async e => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      alert("password don't match");
+    }
+    try {
+      const response = await createAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
+
+      await createUserDocumentFromAuth(response.user, { displayName });
+      resetForm();
+    } catch (error) {
+      if (error.code === 'auth/email-already-in-use') {
+        console.log('Lorem ipsum dolor sit.');
+      }
+      console.log('Lorem ipsum dolor sit amet.', error.message);
+    }
+  };
   return (
     <div>
       <h1>Sign up with ypur email and password</h1>
-      <form onSubmit={() => {}}>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="">Display Name</label>
         <input
           value={displayName}
